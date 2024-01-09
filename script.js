c2878b733fbd02b55245e32f7572000b1582a05b
Mhfairus <97602616+FairusDeveloper@users.noreply.github.com>
2024-01-09 20:44:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+    })
+
+        Toast.fire({
+        icon: 'success',
+        title: 'ยินดีต้อนรับสู่เว็บไซต์'
+})
+
